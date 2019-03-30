68cf357dba22d0073d00f1b98f468c4c9bcd1ad8
Mieszko-I <macmik123@gmail.com>
2019-03-30 17:31:31 +0100

    11.1 done

---
+++ 11,1.js
@@ -0,0 +1,57 @@
+const solve = () => {
+
+    const serial_n = parseInt(document.querySelector('.code').innerText);
+    const power_lvl = [];
+    const top_left = [];
+
+    for (let i = 0; i < 298; i++) {
+
+        for (let j = 0; j < 298; j++) {
+
+            power_lvl.push(countPowerLvl(i, j, serial_n));
+            top_left.push([i+1, j+1]);
+
+        }
+
+    }
+
+    const val = power_lvl.reduce((a,b) => {
+        if(a>b) {
+            return a;
+        } else {
+            return b;
+        }
+    });
+
+    return top_left[power_lvl.indexOf(val)];
+
+}
+
+const countPowerLvl = (x, y, serial) => {
+
+    let power = 0
+
+    for (let i = x; i < x + 3; i++) {
+
+        for (let j = y; j < y + 3; j++) {
+
+            const rack = i+1 + 10;
+            let power_lvl = rack * (j+1);
+            power_lvl += serial;
+            power_lvl *= rack;
+
+            if (power_lvl < 100) {
+                power_lvl = 0;
+            } else {
+                power_lvl = parseInt(power_lvl.toString().charAt(power_lvl.toString().length - 3));
+            }
+
+            power += power_lvl;
+
+        }
+
+    }
+
+    return power;
+
+}
